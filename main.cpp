8a0383f97bff7eeb2075120db0e45c1f2a03c30a
PIETRO <prodrigano.ieu2022@student.ie.edu>
2024-11-30 18:22:05 +0100

    menu display

--- main.cpp
+++ main.cpp
@@ -1,5 +1,36 @@
 #include "Player.hpp"
 #include "Projectile.hpp"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+// Function to display the menu
+void displayMenu() {
+    std::cout << "===================================\n";
+    std::cout << "            SPACE INVADERS         \n";
+    std::cout << "===================================\n\n";
+    std::cout << "Welcome to Space Invaders!\n";
+    std::cout << "---------------------------------\n";
+    std::cout << "Press 'P' to Play Space Invaders!\n";
+    std::cout << "Press 'Q' to Quit.\n";
+}
+
+int main() {
+    // Display the menu
+    displayMenu();
+
+    char choice;
+    std::cin >> choice;
+
+    if (choice == 'Q' || choice == 'q') {
+        std::cout << "Exiting Space Invaders. Goodbye!\n";
+        return 0;
+    } else if (choice != 'P' && choice != 'p') {
+        std::cout << "Invalid choice. Exiting.\n";
+        return 0;
+    }
+
+    return 0;
+}
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Alien Invasion");
